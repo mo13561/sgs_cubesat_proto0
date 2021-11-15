95aec919e362b223c77536f194960bc4d3d86eae
rkchi <rkchi@DESKTOP-MINV229>
2021-11-15 23:06:19 +0000

    Settings for heroku

--- proto/dataView/urls.py
+++ proto/dataView/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("", views.redirectLiveData),
-    path("live/", views.liveData, name="live")
+    path("live/", views.liveData, name="live"),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
 ]
