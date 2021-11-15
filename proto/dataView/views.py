d4325f3617dfe04301e04ff9eda48277bd923989
rkchi <rkchi@DESKTOP-MINV229>
2021-11-15 22:15:52 +0000

    Added live data view push

--- proto/dataView/views.py
+++ proto/dataView/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def liveData(request):
+    input_array = []
+    # input array is list of dictionaries with name, value as two keys.
+    return render(request, "dataView/liveDataView.html", {
+        'input_array': input_array
+    })
