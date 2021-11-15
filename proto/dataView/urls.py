e1de421050bffbbea4b6880e39f5268e38812123
rkchi <rkchi@DESKTOP-MINV229>
2021-11-15 22:52:08 +0000

    Debugged full and random values added

--- proto/dataView/urls.py
+++ proto/dataView/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/", views.redirectLiveData, name="live"),
-    path("/live", views.liveData)
+    path("", views.redirectLiveData),
+    path("live/", views.liveData, name="live")
 ]
--- proto/dataView/views.py
+++ proto/dataView/views.py
@@ -8,7 +8,7 @@ def redirectLiveData(request):
 
 
 def liveData(request):
-    input_array = []
+    input_array = [{'name': 'a', 'value': '1'}, {'name': 'b', 'value': '2.5'}, {'name': 'c', 'value': 'sg'}, {'name': 'd', 'value': '2345'}]
     # input array is list of dictionaries with name, value as two keys.
     return render(request, "dataView/liveDataView.html", {
         'input_array': input_array
